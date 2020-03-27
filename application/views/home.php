9f76943a712236e6eb7e3a15e1038557d017222b
Risma Ayu <risma_ayu_26rpl@student.smktelkom-mlg.sch.id>
2020-03-27 20:11:15 +0700

    first commit

---
+++ application/views/home.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <style>
+      h1{
+        text-align:center;
+      }
+    </style>
+  </head>
+  <body>
+  <h1>Selamat Datang Di Sarpra</h1>
+  <h1>Setelah Meminjam Harap Dikembalikan</h1>
+  </body> 
+</html>
